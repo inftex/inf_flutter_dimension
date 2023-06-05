a432042a490dad1f4d37f3912607a0364959fc0b
baotran@inftech.vn <brightsmile959674@gmail.com>
2023-06-05 22:53:16 +0700

    hot fix parent

--- lib/mixin/dimension_mixin.dart
+++ lib/mixin/dimension_mixin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-mixin DimensionStateMixin on State {
+mixin DimensionStateMixin<T extends StatefulWidget> on State<T> {
   Size get screenSize => MediaQuery.of(context).size;
   double get screenWidth => screenSize.width;
   double get screenHeight => screenSize.height;
